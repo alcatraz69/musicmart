83f93b793adb809df943c9286d3f77a50e7e9073
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-04-19 22:24:23 +0530

    feat - added filter page

---
+++ src/Components/FilterCard/FilterCard.js
@@ -0,0 +1,48 @@
+import styles from './FilterCard.module.css'
+export default function FilterCard(){
+    return(
+        <div className={styles.FilterCard} style={{ display: "flex", flexWrap: "wrap" ,flexDirection: "column"}}>
+            
+          <legend>Sort by price</legend>
+          <label>
+            <input
+            className={styles.radioBtn}
+              type="radio"
+              name="price"
+            //   onClick={() => dispatch({ type: "SORTLOWTOHIGH" })}
+            />
+            low to high
+          </label>
+          <label>
+            <input
+            className={styles.radioBtn}
+              type="radio"
+              name="price"
+            //   onClick={() => dispatch({ type: "SORTHIGHTOLOW" })}
+            />
+            hight to low
+          </label>
+          
+        
+          <legend>Preferences</legend>
+          <label>
+            <input
+              type="checkbox"
+            //   onClick={() => dispatch({ type: "OUTOFSTOCK" })}
+            />
+            inclue out of stock products
+          </label>
+          <label>
+            <input
+              type="checkbox"
+            //   onClick={() => dispatch({ type: "WITHFASTDELIVERY" })}
+            />
+            fast delivery only
+          </label>
+
+
+
+
+        </div>
+    )
+}
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -3,15 +3,15 @@ import { useContext } from 'react';
 import styles from "./Products.module.css"
 import {ProductContext} from '../../store/ProductContext';
 import ProductCard from './ProductCard/ProductCard'
+import FilterCard from '../FilterCard/FilterCard'
 
 export default function Products() {
   const {products}=useContext(ProductContext);
   console.log(products);
   return(
+    <>
+    <FilterCard/>
     <div className={styles.products}>
-      
-     
-                
                     {
                         products&&products.map(({id,name,image,price,rating,hasDiscount,discount
                             ,fastDelivery,inStock,inCart,inWishlist})=>(
@@ -36,5 +36,6 @@ export default function Products() {
             
 
     </div>
+    </>
   ) 
 }
