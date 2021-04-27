a55d77447fb74a32ce378e5208a4d896a5f3ca7e
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-04-27 11:41:44 +0530

    feat - added car summary functionality

--- src/Components/CartPage/cartSummary/cartSummary.js
+++ src/Components/CartPage/cartSummary/cartSummary.js
@@ -1,16 +1,26 @@
 import styles from './cartSummary.module.css'
+import {useContext} from 'react'
+import {ProductContext} from '../../../store/ProductContext'
 
 export default function CartSummary(){
+    const {cartItems} = useContext(ProductContext)
+
+    const totalPrice = cartItems.reduce(
+        (count, item) => count + parseInt(item.price) * item.quantity, 0
+      );
+
+
+
     return(
         <div className={styles.section}>
             <h4>Price Details</h4>
             <div className={styles.priceDetails}>
                 <p>Total MRP</p>
-                <p>Rs. 5,000</p>
+                <p>Rs. {totalPrice}</p>
             </div>
             <div className={styles.priceDetails}>
                 <p>Total Discount</p>
-                <p>-Rs 2,000</p>
+                <p>-Rs 200</p>
             </div>
             <div className={styles.priceDetails}>
                 <p>Convinence Fee</p>
@@ -19,7 +29,7 @@ export default function CartSummary(){
             <hr/>
             <div className={styles.priceDetails}>
                 <p style={{fontSize:"large",fontWeight:"500"}}>Total MRP</p>
-                <p style={{fontSize:"large",fontWeight:"500"}}>Rs. 3,000</p>
+                <p style={{fontSize:"large",fontWeight:"500"}}>Rs. {totalPrice}</p>
             </div>
             <button className={styles.orderBtn}>Place Order</button>
         </div>
